99f1a803215cd5ecb6c7abf5bc171e91b5680d5e
Eric M - (Work) <146989968+Mugiwara-1221@users.noreply.github.com>
2025-07-31 10:15:46 -0700

    Update storyboard.js

--- storyboard.js
+++ storyboard.js
@@ -1,1 +1,11 @@
 
+// Load the colored drawing from localStorage
+const coloredDataURL = localStorage.getItem("coloredCharacter");
+
+// If found, display it in the scene
+if (coloredDataURL) {
+  const img = document.getElementById("coloredCharacter");
+  img.src = coloredDataURL;
+} else {
+  alert("No character image found. Please color your character first.");
+}
