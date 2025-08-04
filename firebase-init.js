405f58aa5ad0328199968334c7c443dfa7dd53b0
Eric M - (Work) <146989968+Mugiwara-1221@users.noreply.github.com>
2025-08-04 12:48:05 -0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,20 @@
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCRO_uKJ5RFU61rOgS7_xmFvGTt-kcw6f4",
+    authDomain: "animationcollab-15353.firebaseapp.com",
+    projectId: "animationcollab-15353",
+    storageBucket: "animationcollab-15353.firebasestorage.app",
+    messagingSenderId: "349523133073",
+    appId: "1:349523133073:web:74c42352875a7419217530"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
