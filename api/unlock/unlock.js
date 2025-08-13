155166e7e56887a9c5c36376f63d49d18e441e19
hidalgoerick <146991011+hidalgoerick@users.noreply.github.com>
2025-08-13 15:38:28 -0700

    Update unlock.js

--- api/unlock/unlock.js
+++ api/unlock/unlock.js
@@ -1,6 +1,18 @@
-{
-  "bindings": [
-    { "authLevel": "anonymous", "type": "httpTrigger", "direction": "in", "name": "req", "methods": [ "delete" ], "route": "session/{code}/lock/{character}" },
-    { "type": "http", "direction": "out", "name": "res" }
-  ]
-}
+const { TableClient } = require("@azure/data-tables");
+
+const STORAGE = process.env.STORAGE_CONNECTION_STRING;
+const LOCKS_TABLE = "locks";
+
+module.exports = async function (context, req) {
+  const code = context.bindingData.code;
+  const character = context.bindingData.character;
+  try {
+    const locks = TableClient.fromConnectionString(STORAGE, LOCKS_TABLE);
+    await locks.createTable();
+    await locks.deleteEntity(code, character); // ignore if missing
+    context.res = { status: 200, jsonBody: { ok: true } };
+  } catch (err) {
+    context.log.error(err);
+    context.res = { status: 500, jsonBody: { error: "unlock_failed" } };
+  }
+};
