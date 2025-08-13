70c1ca283a580fecb652d7bbcc2a1229e2599766
hidalgoerick <146991011+hidalgoerick@users.noreply.github.com>
2025-08-13 15:28:06 -0700

    Update function.js

--- api/session/function.js
+++ api/session/function.js
@@ -1,1 +1,6 @@
-
+{
+  "bindings": [
+    { "authLevel": "anonymous", "type": "httpTrigger", "direction": "in", "name": "req", "methods": [ "post" ], "route": "session" },
+    { "type": "http", "direction": "out", "name": "res" }
+  ]
+}
