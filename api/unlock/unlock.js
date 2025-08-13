0c40af81fea1a61456a0b861a366f5d8ad78a0f5
hidalgoerick <146991011+hidalgoerick@users.noreply.github.com>
2025-08-13 15:38:06 -0700

    Update unlock.js

--- api/unlock/unlock.js
+++ api/unlock/unlock.js
@@ -1,1 +1,6 @@
-
+{
+  "bindings": [
+    { "authLevel": "anonymous", "type": "httpTrigger", "direction": "in", "name": "req", "methods": [ "delete" ], "route": "session/{code}/lock/{character}" },
+    { "type": "http", "direction": "out", "name": "res" }
+  ]
+}
