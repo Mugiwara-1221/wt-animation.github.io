7d9214eeabab132ec80fbe83b83b3e16ab69523f
Eric M - (Work) <146989968+Mugiwara-1221@users.noreply.github.com>
2025-08-28 09:49:08 -0700

    Update grad-config.js

--- js/grad-config.js
+++ js/grad-config.js
@@ -1,1 +1,6 @@
 
+export async function getGradeCaps(gradeId) {
+  const url = 'stories/config/grades.json';
+  const data = await (await fetch(url)).json();
+  return data.grades[gradeId] || data.grades['k-2'];
+}
