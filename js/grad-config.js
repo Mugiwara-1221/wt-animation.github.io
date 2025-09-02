b9eac338ceda37457407451edcb353d56d090841
hidalgoerick <146991011+hidalgoerick@users.noreply.github.com>
2025-09-02 10:37:44 -0700

    Update grad-config.js

--- js/grad-config.js
+++ js/grad-config.js
@@ -3,7 +3,7 @@
   //const url = 'stories/config/grades.json';
   const url = 'https://mugiwara-1221.github.io/wt-animation.github.io/stories/config/grades.json';
   const data = await (await fetch(url)).json();
-  return data.grades[gradeId] || data.grades['k-2'];
+  return data.grades[gradeId] || data.grades['k-2']; fd
 }*/
 
 export async function getGradeCaps(gradeId) {
