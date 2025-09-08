3412111896ceca2e8a3a220d8197f09abecb4b18
hidalgoerick <146991011+hidalgoerick@users.noreply.github.com>
2025-09-08 12:14:46 -0700

    update workflow

--- js/color-map-advanced.js
+++ js/color-map-advanced.js
@@ -264,8 +264,8 @@ export async function colorCharacterFrames({
   for (let n = 1; n <= frameCount; n++) {
     const dataURL = await colorAFrameAdvanced({
       //frame1URL: maskedBaseDataURL,
-      map1CSVURL: `${maskCsvPrefix}}/mask_${n}.csv`,
-      frame2URL: `${framesPath}${n}.png`
+      frame2URL: `${framesPath}${n}.png`,
+      map1CSVURL: `${maskCsvPrefix}}/mask_${n}.csv`
       //map2CSVURL: `${dir}/mask_${n}.csv`,
     });
     results.push({ n, dataURL });
