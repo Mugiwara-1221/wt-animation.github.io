dfd3210ab8d9845330fb914e12e675b973f41197
hidalgoerick <146991011+hidalgoerick@users.noreply.github.com>
2025-09-12 14:31:39 -0700

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -315,6 +315,7 @@ async function matrixToMaskCanvas(mat, srcW, srcH, targetW, targetH){
 
 /* ---------- Frame discovery & export ---------- */
 /*images/frames/tortoise_and_the_hare/frame1/tortoise/tortoise_mask_1.csv*/
+/*Fix this code as the it needs to call upon all forlders! before it was circulating between frames 1 to 5 folders*/
 async function findMaskSets(storyIdDash, charId){
   const storyFolder=resolveStoryFolder(storyIdDash);
   const base=`images/frames/${storyFolder}`; const out=[];
