1d9cd91a98619c68ea66328febeee8623edd5ea0
hidalgoerick <146991011+hidalgoerick@users.noreply.github.com>
2025-09-12 13:22:39 -0700

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -350,6 +350,11 @@ async function sendToStoryboard(){
         mctx.globalCompositeOperation="destination-in"; mctx.drawImage(maskCanvas,0,0);
         mctx.globalCompositeOperation="source-over";
         list.push(masked.toDataURL("image/png"));
+
+        // <--- New download logic here:
+        downloadDataUrl(dataUrl, `${selectedChar}_slide${frame}_mask${i}.png`);
+        // This will prompt the user to download this PNG
+        }
       }
       if(list.length) bySlide[frame]=list;
     }
