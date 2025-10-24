a5cbd1cd07a3b68e32828196458a76979137230c
Josh <josh.sullivan1117@gmail.com>
2025-10-23 17:11:48 -0700

    added many assests for lion and mouse character select

--- js/sprite-select.js
+++ js/sprite-select.js
@@ -29,7 +29,7 @@ const deviceToken = (() => {
 })();
 
 // List any temporarily disabled characters here
-const TEMP_DISABLED = new Set(["bush"]); // ← replace with your data-char id(s)
+const TEMP_DISABLED = new Set(["bush","net"]); // ← replace with your data-char id(s)
 const userid = localStorage.getItem("memberId");
 
 /************ Pixel-accurate hover + click ************/
